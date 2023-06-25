a100575a0b8a7c8f2871ec4051c354a0eb00ee77
Gercius <52923556+Gercius@users.noreply.github.com>
2023-06-25 11:26:41 +0300

    Entity model, DB config.

---
+++ gamezzzzz-be/Models/Game.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace gamezzzzz_be.Models;
+
+public class Game
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("Name")]
+    public string GameName { get; set; } = null!;
+
+    public string ReleaseDate { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public string About { get; set; } = null!;
+
+    public string Genres { get; set; } = null!;
+
+    public string Image { get; set; } = null!;
+}
