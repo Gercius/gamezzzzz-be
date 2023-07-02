8857052fe0045b68d8f7434b7ad1fa47b3de48c4
Gercius <52923556+Gercius@users.noreply.github.com>
2023-07-02 10:32:26 +0300

    Modified database connection settings, CORS for localhost development.

--- gamezzzzz-be/Program.cs
+++ gamezzzzz-be/Program.cs
@@ -1,22 +1,47 @@
+using gamezzzzz_be.Models;
+using gamezzzzz_be.Services;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000",
+                                "http://0.0.0.0:3000",
+                                "http://127.0.0.1:3000",
+                                "http://localhost:5500",
+                                "http://127.0.0.1:5500");
+        });
+});
+
+builder.Services.Configure<GamezzzzzDatabaseSettings>(
+    builder.Configuration.GetSection("GamezzzzzDatabase"));
+
+builder.Services.AddSingleton<GamesService>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
