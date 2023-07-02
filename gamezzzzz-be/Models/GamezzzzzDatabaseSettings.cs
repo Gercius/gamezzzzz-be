e9bd9a9f34a5d14223f86c8aa07e621225cfd767
Gercius <52923556+Gercius@users.noreply.github.com>
2023-07-02 10:30:06 +0300

    Game model, database settings

---
+++ gamezzzzz-be/Models/GamezzzzzDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace gamezzzzz_be.Models;
+
+public class GamezzzzzDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string GamesCollectionName { get; set; } = null!;
+}
