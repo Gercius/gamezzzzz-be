0dac960836cb02fc13543b3143ac0bea7c4a83e2
Gercius <52923556+Gercius@users.noreply.github.com>
2023-07-08 11:39:22 +0300

    Implemented pagination

--- gamezzzzz-be/Services/GamesService.cs
+++ gamezzzzz-be/Services/GamesService.cs
@@ -4,6 +4,7 @@ using MongoDB.Driver;
 
 namespace gamezzzzz_be.Services;
 
+
 public class GamesService
 {
     private readonly IMongoCollection<Game> _gamesCollection;
@@ -21,8 +22,21 @@ public class GamesService
             gamezzzzzDatabaseSettings.Value.GamesCollectionName);
     }
 
-    public async Task<List<Game?>> GetAsync() =>
-        await _gamesCollection.Find(_ => true).ToListAsync();
+    public async Task<List<Game?>> GetGames(int page, int pageSize)
+    {
+        int skipCount = (page - 1) * pageSize;
+
+        List<Game> games = await _gamesCollection
+            .Find(_ => true)
+            .Skip(skipCount)
+            .Limit(pageSize)
+            .ToListAsync();
+
+        return games;
+    }
+
+    //public async Task<List<Game?>> GetAsync() =>
+    //    await _gamesCollection.Find(_ => true).ToListAsync();
 
     public async Task<Game?> GetAsync(string id) =>
         await _gamesCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
